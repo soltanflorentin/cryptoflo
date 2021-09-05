b742a246e1f8e444ba59918b4b2bbbad5ec4b931
Florentin Soltan <sflorentindev@gmail.com>
2021-09-05 09:02:41 +0100

    deploy to git hub 5 Sept

--- src/StartPage/contentStartPage.js
+++ src/StartPage/contentStartPage.js
@@ -1,9 +1,11 @@
 import TestTable from "./tableStartPage"
+import TestTable2 from "./tableStartPageBootstarp"
 
 const ContentMainPage = () => {
     return ( 
         <div id="contentMainPage">
             <TestTable />
+            {/* <TestTable2 /> */}
         </div>
      );
 }
--- src/StartPage/navBarStartPage.js
+++ src/StartPage/navBarStartPage.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import poz from './favicon.jpeg';
 
 const NavBar = () => {
     return ( 
@@ -23,7 +24,7 @@ const NavBar = () => {
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                            <a class="navbar-brand" href="#"><img src="./poz.jpg" alt="logo"/></a>
+                            <a class="navbar-brand" href="#"><img src={poz} class="logoImg" alt="logo"/></a>
                             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                             <li class="nav-item active">
                                 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
@@ -32,9 +33,15 @@ const NavBar = () => {
                                 <a class="nav-link" href="#">Link</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link disabled" href="#">Disabled</a>
+                                <a class="nav-link" href="#">Disabled</a>
                             </li>
                             </ul>
+                            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                                    <li class="nav-item">
+                                        BitFlo 
+                                    </li>
+                            </ul>
+                            
                             <form class="form-inline my-2 my-lg-0">
                             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
                             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
